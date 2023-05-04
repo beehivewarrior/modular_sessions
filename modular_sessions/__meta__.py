977f6414eb9dd60d51c85c852d0ea62779f2cbfb
Jordan Bigelow <jordan@jordanbigelow.com>
2023-05-04 13:21:55 -0600

    Updated Version

--- modular_sessions/__meta__.py
+++ modular_sessions/__meta__.py
@@ -6,6 +6,6 @@ __name__ = 'modular_sessions'
 __author__ = 'Jordan Bigelow'
 __email__ = ['jordan+projects@jordanbigelow.com', ]
 __url__ = "https://github.com/beehivwarrior/modular_sessions"
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __license__ = 'None'
 __description__ = 'A modular session management system for FastAPI.'
