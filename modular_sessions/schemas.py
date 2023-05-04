f5d6247c780d5ff3436fabf96bfad0248a867b5f
Jordan Bigelow <jordan@jordanbigelow.com>
2023-05-04 16:04:35 -0600

    Removed Same Site

--- modular_sessions/schemas.py
+++ modular_sessions/schemas.py
@@ -14,7 +14,6 @@ class SessionCookieParameters(BaseModel):
 
     path = "/"
     max_age: int = Field(3600, alias="max-age", title="max-age")
-    same_site: str = Field("lax", alias="same-site")
 
 
 class UserSession(BaseModel):
